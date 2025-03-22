d01dd666f336db01647aa178a367a836f0618858
Yazan <muqbel.yazan@gmail.com>
2025-03-22 03:39:16 +0300

    Fixed SEO metadata & tracking script issue

--- components/SEOHead.js
+++ components/SEOHead.js
@@ -5,6 +5,11 @@ const SEOHead = ({ title, description, url, image }) => {
   return (
     <>
       <Head>
+
+        {/* ✅ Google Verification Codeß */}
+      <meta name="google-site-verification" content="sTltP8i6mN_Tp2Yl46U_iub-cWD8cApOoakNho0ocyE" />
+
+
         {/* ✅ Primary Meta Tags */}
         <title>{title}</title>
         <meta name="description" content={description} />
